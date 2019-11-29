8df24fdcef84d4670d51e73166968b647524fe45
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2019-11-29 14:52:37 -0800

    added set_nested_dict, get_nested_dict to tools

--- distgen/tools.py
+++ distgen/tools.py
@@ -263,5 +263,46 @@ def unflatten_dict(d, sep=':', prefix=''):
         
         d1[klist[-1]] = vv
     return dd
+def set_nested_dict(dd, flatkey, val, sep=':', prefix=''):
+    """
+    Set a value inside nested dicts using a key string. 
+    Example:
+        dd = {'key1':{'key2':{'key3':9}}}
+        set_nested_dict(dd, 'P:key1:key2:key3', 4, prefix='P')
+        
+        will set dd in place as:
+            {'key1': {'key2': {'key3': 4}}}
+        
+    
+    """
+    if flatkey.startswith(prefix+sep):
+        flatkey=flatkey[len(prefix+sep):]    
+    keys = flatkey.split(':')
+    d = dd
+    # Go through nested dicts
+    for k in keys[0:-1]:
+        d = d[k]
+    final_key = keys[-1]
+    # Set
+    if final_key in d:
+        d[final_key] = val
+    else:
+        print(f'Error: flat key {flatkey} key does not exist:', final_key)
+
+def get_nested_dict(dd, flatkey, sep=':', prefix='distgen'):
+    """
+    Gets the value in a nested dict from a flattened key.
+    See: flatten_dict
+    """
+    if flatkey.startswith(prefix+sep):
+        flatkey=flatkey[len(prefix+sep):]
+    keys = flatkey.split(':')
+    d = dd
+    # Go through nested dicts
+    for k in keys:
+        d = d[k]
+    return d
+
+
 
 
