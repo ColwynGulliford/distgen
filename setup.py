f5d61959ac476a9bb78255b59aa08bcf00f672df
Colwyn Gulliford <cg248@cornell.edu>
2020-01-09 22:54:09 -0500

    Packaging
    Making distgen into package

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+from os import path, environ
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
+    requirements = f.read().split()
+
+
+
+setup(
+    name='distgen',
+    version = 'v0.1.0',
+    #packages = ['distgen'],
+    packages=find_packages(), 
+    package_dir={'distgen':'distgen'},
+    url='https://github.com/ColwynGulliford/distgen',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.6'
+)
