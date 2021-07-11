d574d7b794508662a7e6f1c7f2fa163999ccdccd
QFGlenn <sum.mannus@gmail.com>
2021-07-11 12:51:43 -0700

    integrating pdf reader beta

--- distgen/tools.py
+++ distgen/tools.py
@@ -11,7 +11,8 @@ import json
 from hashlib import blake2b
 import datetime
 import os
-
+from pdf2image import convert_from_path
+import yaml
 
 # HELPER FUNCTIONS:
 
@@ -368,6 +369,28 @@ def read_image_file(filename, rgb_weights = [0.2989, 0.5870, 0.1140]):
 
     return greyscale
 
+def read_pdf_file(filename):
+    config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'examples')
+
+    pdf = convert_from_path(config_file_path +'/data/'+filename)
+
+    for q in range(len(pdf)):
+        pdf[q].save('png.pdf.test.page' + str(q) + '.png','PNG')
+        tempDictionary = [{'n_particle': 1000000,
+            'output': {'file': 'rad.uniform.out.txt', 'type': 'gpt'},
+            'random_type': 'hammersley', 
+            'start': {'MTE': {'units': 'meV', 'value': 150}, 'type': 'cathode'},
+            'total_charge': {'units': 'pC', 'value': 10},
+            'xy_dist': {'file': 'png.pdf.test.page' + str(q) + '.png', 
+                'max_x': {'units': 'mm', 'value': 1},
+                'max_y': {'units': 'mm', 'value': 1},
+                'min_x': {'units': 'mm', 'value': -1},
+                'min_y': {'units': 'mm', 'value': -1},
+                'threshold': 0.0,
+                'type': 'file2d'}}]
+    
+        with open((config_file_path +'/data/' + 'pdf.test.page' + str(q) + '.in.yaml'),'w') as file:
+            documents = yaml.dump(tempDictionary, file)
 
 
 #--------------------------------------------------------------
