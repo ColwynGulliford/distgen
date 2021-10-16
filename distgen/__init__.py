8fdda2008906d41b08ae8f37253092e007af02f2
Colwyn Gulliford <cg248@cornell.edu>
2021-10-16 12:46:56 -0400

    Versioneer

--- distgen/__init__.py
+++ distgen/__init__.py
@@ -1,1 +1,3 @@
 from .generator import Generator
+from . import _version
+__version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-
+import versioneer
 from setuptools import setup, find_packages
 from os import path, environ
 
@@ -9,13 +9,14 @@ with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
 
 setup(
     name='distgen',
-    version = '0.4.5',
-    packages=find_packages(), 
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(), 
+    packages=find_packages(),  # This is the old package
     package_dir={'distgen':'distgen'},
     url='https://github.com/ColwynGulliford/distgen',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=requirements,
     include_package_data=True,
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
